029f5fb5202090e16850f68dd712f5e5a5ce1845
samfreund <samf.236@proton.me>
2026-09-08 18:48:07 -0500

    update sim
    Signed-off-by: samfreund <samf.236@proton.me>

--- photon-lib/py/photonlibpy/simulation/visionSystemSim.py
+++ photon-lib/py/photonlibpy/simulation/visionSystemSim.py
@@ -309,7 +309,7 @@ class VisionSystemSim:
             timestampNt = optTimestamp
             latency = camSim.prop.estLatency()
             # the image capture timestamp in seconds of this result
-            timestampCapture = timestampNt - latency / 1.0e3
+            timestampCapture = timestampNt - latency  # latency is already in seconds
 
             # use camera pose from the image capture timestamp
             lateRobotPose = self.getRobotPose(timestampCapture)
