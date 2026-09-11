0ee01897267e79a2b5b660a450fd3b3bbbf4f56d
samfreund <samf.236@proton.me>
2026-09-11 13:50:18 -0500

    change to nanoseconds
    Signed-off-by: samfreund <samf.236@proton.me>

--- photon-lib/py/test/photonPoseEstimator_test.py
+++ photon-lib/py/test/photonPoseEstimator_test.py
@@ -60,7 +60,7 @@ def test_lowestAmbiguityStrategy():
     aprilTags = fakeAprilTagFieldLayout()
     cameraOne = PhotonCameraInjector()
     cameraOne.result = PhotonPipelineResult(
-        int(11 * 1e6),
+        int(11 * 1e9),
         [
             PhotonTrackedTarget(
                 3.0,
@@ -129,7 +129,7 @@ def test_lowestAmbiguityStrategy():
                 0.4,
             ),
         ],
-        metadata=PhotonPipelineMetadata(0, int(2 * 1e6), 0),
+    metadata=PhotonPipelineMetadata(0, int(2 * 1e6), 0),  # 2ms latency in nanoseconds
         multitagResult=None,
     )
 
@@ -267,7 +267,7 @@ def test_multiTagOnCoprocStrategy():
                 0.7,
             )
         ],
-        metadata=PhotonPipelineMetadata(0, int(2 * 1e6), 0),
+    metadata=PhotonPipelineMetadata(0, int(2 * 1e6), 0),  # 2ms latency in nanoseconds
         multitagResult=MultiTargetPNPResult(
             PnpResult(Transform3d(1, 3, 2, Rotation3d()))
         ),
