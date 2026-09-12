2321a7903b129f6db8473f1c1219df86b592f2bf
samfreund <samf.236@proton.me>
2026-09-12 01:40:13 -0500

    formatting, whee
    Signed-off-by: samfreund <samf.236@proton.me>

--- photon-lib/py/test/photonPoseEstimator_test.py
+++ photon-lib/py/test/photonPoseEstimator_test.py
@@ -129,9 +129,7 @@ def test_lowestAmbiguityStrategy():
                 0.4,
             ),
         ],
-        metadata=PhotonPipelineMetadata(
-            0, int(2e6), 0
-        ),  # 2ms latency in nanoseconds
+        metadata=PhotonPipelineMetadata(0, int(2e6), 0),  # 2ms latency in nanoseconds
         multitagResult=None,
     )
 
@@ -269,9 +267,7 @@ def test_multiTagOnCoprocStrategy():
                 0.7,
             )
         ],
-        metadata=PhotonPipelineMetadata(
-            0, int(2e6), 0
-        ),  # 2ms latency in nanoseconds
+        metadata=PhotonPipelineMetadata(0, int(2e6), 0),  # 2ms latency in nanoseconds
         multitagResult=MultiTargetPNPResult(
             PnpResult(Transform3d(1, 3, 2, Rotation3d()))
         ),
